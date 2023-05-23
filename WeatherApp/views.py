40911ed0e883be12131535ece9f84ee051348762
Anshuk-Mishra <77469638+Anshuk-Mishra@users.noreply.github.com>
2023-05-23 19:00:02 +0530

    Version 1.1

--- WeatherApp/views.py
+++ WeatherApp/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+import requests
+import creds
 # Create your views here.
+
+def fetch(request):
+    response = get_api_data(creds.api)
+    return render(request,'index.html',{'response':response})
+
+def get_api_data(api_key):
+    url = creds.url  # Replace with the actual API endpoint
+    headers = {
+        "Authorization": "API_KEY {}".format(api_key),
+        "Content-Type": "application/json"  # Adjust the content type if needed
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json
+        # Process the data as needed
+        return data
+    else:
+        # Handle the error response
+        return None
